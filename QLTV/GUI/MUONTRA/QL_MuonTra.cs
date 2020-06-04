39bbe9452d9cfb950f8baa6e2820ee637219b53f
vuxuanhung1999mta@gmail.com <vuxuanhung1999mta@gmail.com>
2020-06-05 01:16:16 +0700

    Hung: update MuonTra

--- QLTV/GUI/MUONTRA/QL_MuonTra.cs
+++ QLTV/GUI/MUONTRA/QL_MuonTra.cs
@@ -6,10 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QLTV.DAL;
 using System.Windows.Forms;
 using static QLTV.DTO.MuonTra;
 using static QLTV.DAL.MuonTra_DAL;
-using QLTV.DAL;
+
 
 namespace QLTV.GUI.MUONTRA
 {
@@ -20,6 +21,27 @@ namespace QLTV.GUI.MUONTRA
             InitializeComponent();
         }
 
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //load
+        private void QL_MuonTra_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = DAL.DataProvider.Instance.ExecuteQuery("select * from PhieuMuon_View");
+            dataGridView2.DataSource = DAL.DataProvider.Instance.ExecuteQuery("select * from PhieuTra_View");
+        }
         private void label7_Click(object sender, EventArgs e)
         {
 
@@ -32,17 +54,39 @@ namespace QLTV.GUI.MUONTRA
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //DAL.MuonTra_DAL.Instance.GetPhieuMuonByCategoryID(17150035);
-          
             dataGridView1.DataSource = MuonTra_DAL.Instance.GetListPhieuMuon();
+        }
 
 
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int currentID;
+            currentID = dataGridView1.CurrentRow.Index;
+            //lấy dòng hiện tại
+            tbMaThe1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            tbNgayMuon1.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            tbNgayHanTra1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            tbCuonSach1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            tbTenSach1.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            tbNhanVien1.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
 
         }
 
-        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void label14_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int currentID;
+            currentID = dataGridView2.CurrentRow.Index;
+            //lấy dòng hiện tại
+            tbMaThe2.Text = dataGridView2.CurrentRow.Cells[1].Value.ToString();
+            tbNgayTra2.Text = dataGridView2.CurrentRow.Cells[2].Value.ToString();
+            tbCuonSach2.Text = dataGridView2.CurrentRow.Cells[3].Value.ToString();
+            tbTenSach2.Text = dataGridView2.CurrentRow.Cells[4].Value.ToString();
+            tbNhanVien2.Text = dataGridView2.CurrentRow.Cells[5].Value.ToString();
         }
     }
 }
